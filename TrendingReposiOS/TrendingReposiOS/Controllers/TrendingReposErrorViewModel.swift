9e560dbffa08dec1e8c55c8736949c2c030b3fe0
Daniel Tischenko <daniil.tishenko.99@gmail.com>
2023-01-05 17:59:59 +0200

    `TrendingReposViewController` displays an error state as per design

---
+++ TrendingReposiOS/TrendingReposiOS/Controllers/TrendingReposErrorViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  TrendingReposErrorViewModel.swift
+//  TrendingReposiOS
+//
+//  Created by Daniel Tischenko on 04.01.2023.
+//
+
+public struct TrendingReposErrorViewModel {
+    public let title: String?
+    public let message: String?
+}
