df8ef6563ae48d8f098b433a3672e5b750c39b53
Daniel Tischenko <daniil.tishenko.99@gmail.com>
2023-01-07 21:59:26 +0200

    Create `TrendingReposApp` xcode project

---
+++ TrendingReposApp/TrendingReposApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrendingReposApp
+//
+//  Created by Daniel Tischenko on 07.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
