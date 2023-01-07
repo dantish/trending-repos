39590f50eee8a3247660b9b706f3a766421efc20
Daniel Tischenko <daniil.tishenko.99@gmail.com>
2023-01-07 18:39:21 +0200

    Make `TrendingRepoViewModel`'s `description` and `language` properties optional

--- TrendingReposiOS/TrendingRepos/Presentation/TrendingRepoViewModel.swift
+++ TrendingReposiOS/TrendingRepos/Presentation/TrendingRepoViewModel.swift
@@ -9,8 +9,8 @@ import Foundation
 
 public struct TrendingRepoViewModel<AvatarImage> {
     public let name: String
-    public let description: String
-    public let language: String
+    public let description: String?
+    public let language: String?
     public let starsCount: String
     public let ownerName: String
     public let ownerAvatar: AvatarImage
