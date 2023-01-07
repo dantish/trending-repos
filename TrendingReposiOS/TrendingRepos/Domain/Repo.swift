a1b7cf0a693a616cb157efbeccc608593bf33369
Daniel Tischenko <daniil.tishenko.99@gmail.com>
2023-01-07 14:32:40 +0200

    Make `Repo.description` and `Repo.language` properties optional

--- TrendingReposiOS/TrendingRepos/Domain/Repo.swift
+++ TrendingReposiOS/TrendingRepos/Domain/Repo.swift
@@ -10,12 +10,12 @@ import Foundation
 public struct Repo: Hashable {
     public let id: UUID
     public let name: String
-    public let description: String
-    public let language: String
+    public let description: String?
+    public let language: String?
     public let starsCount: Int
     public let owner: RepoOwner
 
-    public init(id: UUID, name: String, description: String, language: String, starsCount: Int, owner: RepoOwner) {
+    public init(id: UUID, name: String, description: String?, language: String?, starsCount: Int, owner: RepoOwner) {
         self.id = id
         self.name = name
         self.description = description
