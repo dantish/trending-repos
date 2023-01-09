055a1d74f5bcd5fa5bcf5e938994c8ee6b5a56e9
Daniel Tischenko <daniil.tishenko.99@gmail.com>
2023-01-09 06:04:12 +0200

    `ReposMapper.map` delivers items on 200 HTTP response with valid JSON items

--- TrendingRepos/TrendingRepos/API/ReposMapper.swift
+++ TrendingRepos/TrendingRepos/API/ReposMapper.swift
@@ -11,7 +11,54 @@ public final class ReposMapper {
     private struct Root: Decodable {
         private let items: [Item]
 
-        private struct Item: Decodable {}
+        private struct Item: Decodable {
+            let id: Int
+            let name: String
+            let description: String?
+            let language: String?
+            let starsCount: Int
+            let owner: Owner
+
+            enum CodingKeys: String, CodingKey {
+                case id
+                case name
+                case description
+                case language
+                case starsCount = "stargazers_count"
+                case owner
+            }
+
+            var repo: Repo {
+                Repo(
+                    id: id,
+                    name: name,
+                    description: description,
+                    language: language,
+                    starsCount: starsCount,
+                    owner: RepoOwner(
+                        id: owner.id,
+                        username: owner.login,
+                        avatarUrl: owner.avatarUrl
+                    )
+                )
+            }
+        }
+
+        private struct Owner: Decodable {
+            let id: Int
+            let login: String
+            let avatarUrl: URL
+
+            enum CodingKeys: String, CodingKey {
+                case id
+                case login
+                case avatarUrl = "avatar_url"
+            }
+        }
+
+        var repos: [Repo] {
+            items.map(\.repo)
+        }
     }
 
     public enum Error: Swift.Error {
@@ -19,11 +66,11 @@ public final class ReposMapper {
     }
 
     public static func map(_ data: Data, from response: HTTPURLResponse) throws -> [Repo] {
-        guard response.isOK, let _ = try? JSONDecoder().decode(Root.self, from: data) else {
+        guard response.isOK, let root = try? JSONDecoder().decode(Root.self, from: data) else {
             throw Error.invalidData
         }
 
-        return []
+        return root.repos
     }
 }
 
--- TrendingRepos/TrendingReposTests/API/ReposMapperTests.swift
+++ TrendingRepos/TrendingReposTests/API/ReposMapperTests.swift
@@ -37,4 +37,73 @@ class ReposMapperTests: XCTestCase {
         XCTAssertEqual(result, [])
     }
 
+    func test_map_deliversItemsOn200HTTPResponseWithJSONItems() throws {
+        let item1 = makeItem(
+            id: uniqueID(),
+            name: "a name",
+            starsCount: 0,
+            ownerId: uniqueID(),
+            ownerUsername: "a username",
+            ownerAvatarUrl: URL(string: "http://a-url.com")!
+        )
+
+        let item2 = makeItem(
+            id: uniqueID(),
+            name: "another name",
+            description: "another description",
+            language: "another language",
+            starsCount: 100,
+            ownerId: uniqueID(),
+            ownerUsername: "another username",
+            ownerAvatarUrl: URL(string: "http://another-url.com")!
+        )
+
+        let json = try! JSONSerialization.data(withJSONObject: ["items": [item1.json, item2.json]])
+
+        let result = try ReposMapper.map(json, from: HTTPURLResponse(statusCode: 200))
+
+        XCTAssertEqual(result, [item1.model, item2.model])
+    }
+
+    // MARK: - Helpers
+
+    private func makeItem(
+        id: Int,
+        name: String,
+        description: String? = nil,
+        language: String? = nil,
+        starsCount: Int,
+        ownerId: Int,
+        ownerUsername: String,
+        ownerAvatarUrl: URL
+    ) -> (model: Repo, json: [String: Any]) {
+        let item = Repo(
+            id: id,
+            name: name,
+            description: description,
+            language: language,
+            starsCount: starsCount,
+            owner: RepoOwner(
+                id: ownerId,
+                username: ownerUsername,
+                avatarUrl: ownerAvatarUrl
+            )
+        )
+
+        let json = [
+            "id": id,
+            "name": name,
+            "description": description as Any,
+            "language": language as Any,
+            "stargazers_count": starsCount,
+            "owner": [
+                "id": ownerId,
+                "login": ownerUsername,
+                "avatar_url": ownerAvatarUrl.absoluteString
+            ]
+        ].compactMapValues { $0 }
+
+        return (item, json)
+    }
+
 }
