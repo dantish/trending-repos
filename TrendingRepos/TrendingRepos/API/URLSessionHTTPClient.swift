67a98b312b65744d6c43df0ffb64dc7a83fd58f9
Daniel Tischenko <daniil.tishenko.99@gmail.com>
2023-01-09 05:04:31 +0200

    `URLSessionHTTPClient.getFromURL` succeeds on `HTTPURLResponse` with data

--- TrendingRepos/TrendingRepos/API/URLSessionHTTPClient.swift
+++ TrendingRepos/TrendingRepos/API/URLSessionHTTPClient.swift
@@ -17,8 +17,16 @@ public final class URLSessionHTTPClient: HTTPClient {
     private struct UnexpectedValuesRepresentation: Error {}
 
     public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
-        session.dataTask(with: url) { _, _, error in
-            completion(.failure(error ?? NSError(domain: "any domain", code: 0)))
+        session.dataTask(with: url) { data, response, error in
+            completion(Result {
+                if let error {
+                    throw error
+                } else if let data, let response = response as? HTTPURLResponse {
+                    return (data, response)
+                } else {
+                    throw UnexpectedValuesRepresentation()
+                }
+            })
         }.resume()
     }
 }
